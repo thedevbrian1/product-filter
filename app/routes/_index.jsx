3c40b387bd6a007d6f690aaeafc0789a8087dc8d
thedevbrian1 <brayomwas95@gmail.com>
2024-12-03 13:16:49 +0300

    Use search params to store state

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,6 @@
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useSubmit } from "@remix-run/react";
 
 import { getProducts } from "../models/product";
-import { useState } from "react";
 
 export const meta = () => {
   return [
@@ -10,8 +9,27 @@ export const meta = () => {
   ];
 };
 
-export async function loader() {
+export async function loader({ request }) {
+  let url = new URL(request.url);
+  let searchParams = url.searchParams;
+
+  let brands = searchParams.getAll("brand");
+
   let result = await getProducts();
+
+  // Filter the products if there are search params. (If someone checked the checkbox). Otherwise return all products
+  let filteredProducts;
+  if (brands.length > 0) {
+    filteredProducts = Array.from(result)
+      .filter((item) => brands.includes(item.model))
+      .map((item) => ({
+        ...item,
+        _id: item._id.toString(),
+      }));
+
+    return filteredProducts;
+  }
+
   let products = Array.from(result).map((item) => ({
     ...item,
     _id: item._id.toString(),
@@ -23,21 +41,17 @@ export async function loader() {
 export default function Index() {
   let products = useLoaderData();
 
-  // let [samsungFilter, setSamsungFilter] = useState("");
-  // let [sonyFilter, setSonyFilter] = useState("");
-  // let [toshibaFilter, setToshibaFilter] = useState("");
-  // let [lgFilter, setLgFilter] = useState("");
-  // let [appleFilter, setAppleFilter] = useState("");
+  let submit = useSubmit();
 
-  let [filterArray, setFilterArray] = useState([]);
+  // let [filterArray, setFilterArray] = useState([]);
 
-  console.log({ filterArray });
+  // console.log({ filterArray });
 
   let filters = ["Samsung", "Sony", "Toshiba", "LG", "Apple"];
 
-  let filteredProducts = products.filter((item) =>
-    filterArray.includes(item.model)
-  );
+  // let filteredProducts = products.filter((item) =>
+  //   filterArray.includes(item.model)
+  // );
 
   return (
     <main>
@@ -45,24 +59,32 @@ export default function Index() {
         <div className="w-80 bg-gray-300 text-gray-800 h-screen pl-4">
           {/* Filters */}
           <h2 className="mt-4">Filter by</h2>
-          <ul className="flex flex-col gap-2 mt-4">
-            {filters.map((filter, index) => (
-              <Filter
-                key={index}
-                title={filter}
-                name="brand"
-                value={filter}
-                filterArray={filterArray}
-                setFilterArray={setFilterArray}
-              />
-            ))}
-          </ul>
+          <Form
+            method="get"
+            onChange={(event) => {
+              console.log({ currentTarget: event.currentTarget });
+              submit(event.currentTarget);
+            }}
+          >
+            <ul className="flex flex-col gap-2 mt-4">
+              {filters.map((filter, index) => (
+                <Filter
+                  key={index}
+                  title={filter}
+                  name="brand"
+                  value={filter}
+                  // filterArray={filterArray}
+                  // setFilterArray={setFilterArray}
+                />
+              ))}
+            </ul>
+          </Form>
         </div>
         <div className="w-full px-12 text-gray-200">
           {/* Products */}
           <h1 className="mt-8 text-4xl font-semibold">Products</h1>
           <ul className="grid grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-            {filterArray.length > 0
+            {/* {brands.length > 0
               ? filteredProducts.map((item) => (
                   <li key={item._id}>
                     <Product product={item} />
@@ -72,7 +94,12 @@ export default function Index() {
                   <li key={item._id}>
                     <Product product={item} />
                   </li>
-                ))}
+                ))} */}
+            {products.map((item) => (
+              <li key={item._id}>
+                <Product product={item} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -88,16 +115,16 @@ function Filter({ title, name, value, filterArray, setFilterArray }) {
           type="checkbox"
           name={name}
           value={value}
-          onChange={() => {
-            // If the value is present in the array, remove it, else add it to the array
-            if (filterArray.includes(value)) {
-              let index = filterArray.indexOf(value);
-              filterArray.splice(index, 1);
-              setFilterArray([...filterArray]);
-            } else {
-              setFilterArray([...filterArray, value]);
-            }
-          }}
+          // onChange={() => {
+          //   // If the value is present in the array, remove it, else add it to the array
+          //   // if (filterArray.includes(value)) {
+          //   //   let index = filterArray.indexOf(value);
+          //   //   filterArray.splice(index, 1);
+          //   //   setFilterArray([...filterArray]);
+          //   // } else {
+          //   //   setFilterArray([...filterArray, value]);
+          //   // }
+          // }}
         />
         {title}
       </label>
